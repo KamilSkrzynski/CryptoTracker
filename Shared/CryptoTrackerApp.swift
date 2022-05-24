548a6fe1fad426dba90cd59949f363e487051238
KamilSkrzynski <kamilskrzynski95@gmail.com>
2022-05-24 18:43:48 +0200

    Initial commit

---
+++ Shared/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  Shared
+//
+//  Created by Kamil Skrzyński on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
